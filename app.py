c95437aed360a6fba33519d06fefe79061ef3325
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-09-23 16:26:18 +0200

    Print the Request object essentials

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 
 app = Flask(__name__)
@@ -6,6 +6,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    # print(f'headers: {request.headers}')
+    # print(f'method: {request.method}')
+    # print(f'path: {request.path}')
+    # print(f'url: {request.url}')
+
+    print('Authorization header: ' + request.headers['Authorization'])
+    print('Request content type: ', request.headers['Content-type'])
+    print('Request body: ', request.json)
+    print('Single key in request body: ', request.json['age'])
+    print('Single key in request body - with get method: ', request.json.get('name'))
+
     return 'Hello from Flask!'
 
 
