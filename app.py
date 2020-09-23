9407bfdfd57e92673eeb1ddbff04b7dcf0190747
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-09-23 16:42:43 +0200

    Create first blog endpoint - GET

--- app.py
+++ app.py
@@ -1,29 +1,39 @@
 from flask import Flask, request, make_response, jsonify
 
+POSTS = [
+    {
+        'id': 1,
+        'title': 'title 1',
+        'text': 'text 1'
+    },
+    {
+        'id': 2,
+        'title': 'title 2',
+        'text': 'text 2'
+    },
+    {
+        'id': 3,
+        'title': 'title 3',
+        'text': 'text 3'
+    },
+    {
+        'id': 4,
+        'title': 'title 4',
+        'text': 'text 4'
+    }
+]
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    # print(f'headers: {request.headers}')
-    # print(f'method: {request.method}')
-    # print(f'path: {request.path}')
-    # print(f'url: {request.url}')
+@app.route('/posts')
+def items():
+    response_data = {
+        'success': True,
+        'data': POSTS
+    }
 
-    # print('Authorization header: ' + request.headers['Authorization'])
-    # print('Request content type: ', request.headers['Content-Type'])
-    # print('Request body: ', request.json)
-    # print('Single key in request body: ', request.json['age'])
-    # print('Single key in request body - with get method: ', request.json.get('name'))
-
-    # html_response = '<h1>Hello from Flask!</h1>'
-    # response = make_response([{'id': 1, 'title': 'Title'}], 200)  # code 200 is returned by default - can be skipped
-    # response.headers['Content-Type'] = 'application/json'
-    response = jsonify([{'id': 1, 'title': 'Title'}])
-    response.status_code = 201
-
-    return response
+    return jsonify(response_data)
 
 
 if __name__ == '__main__':
