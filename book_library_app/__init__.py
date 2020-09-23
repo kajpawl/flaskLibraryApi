0ff016f523da6d07fc9ca5081b3c0ed9e2cfaace
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-09-23 17:51:19 +0200

    Sketch functions for 'authors' endpoints

--- book_library_app/__init__.py
+++ book_library_app/__init__.py
@@ -4,7 +4,4 @@ from config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
 
-
-@app.route('/')
-def index():
-    return 'Hello from Flask!'
+from book_library_app import authors
