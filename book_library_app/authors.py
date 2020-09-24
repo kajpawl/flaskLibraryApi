305c2653bd46f8ed1295bf44455d75de4034e3ae
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-09-24 22:57:35 +0200

    Install marshmallow, implement get authors

--- book_library_app/authors.py
+++ book_library_app/authors.py
@@ -1,17 +1,21 @@
 from flask import jsonify
 from book_library_app import app
+from book_library_app.models import Author, AuthorSchema
 
 
 @app.route('/api/v1/authors', methods=['GET'])
 def get_authors():
+    authors = Author.query.all()
+    author_schema = AuthorSchema(many=True)
     return jsonify({
         'success': True,
-        'data': 'Get all authors'
+        'data': author_schema.dump(authors),
+        'number_of_records': len(authors)
     })
 
 
 @app.route('/api/v1/authors/<int:author_id>', methods=['GET'])
-def get_author(author_id):
+def get_author(author_id: int):
     return jsonify({
         'success': True,
         'data': f'Get single author with id {author_id}'
@@ -27,7 +31,7 @@ def create_author():
 
 
 @app.route('/api/v1/authors/<int:author_id>', methods=['PUT'])
-def update_author(author_id):
+def update_author(author_id: int):
     return jsonify({
         'success': True,
         'data': f'Author with id {author_id} has been updated'
