d83c2bb154695262a43e73452072c2714a28afd3
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-09-24 17:09:15 +0200

    Add Author model

--- book_library_app/__init__.py
+++ book_library_app/__init__.py
@@ -7,8 +7,5 @@ app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 
-results = db.session.execute('show databases')
-for row in results:
-    print(row)
-
 from book_library_app import authors
+from book_library_app import models
