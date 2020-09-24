56903e31253a9a26b137665b630f455b1ec2163f
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-09-24 16:49:46 +0200

    Connect to database

--- book_library_app/__init__.py
+++ book_library_app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+
+results = db.session.execute('show databases')
+for row in results:
+    print(row)
 
 from book_library_app import authors
