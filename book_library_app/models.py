31ff11d728afc4776b2755769a72033c5923831b
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-09-26 22:35:17 +0200

    Add get all books command

--- book_library_app/models.py
+++ book_library_app/models.py
@@ -38,17 +38,37 @@ class Book(db.Model):
     def __repr__(self):
         return f'{self.title} - {self.author.first_name} {self.author.last_name}'
 
+    @staticmethod
+    def additional_validation(param: str, value: str) -> str:
+        return value
+
 
 class AuthorSchema(Schema):
     id = fields.Integer(dump_only=True)
     first_name = fields.String(required=True, validate=validate.Length(max=50))
     last_name = fields.String(required=True, validate=validate.Length(max=50))
     birth_date = fields.Date('%d-%m-%Y', required=True)
+    books = fields.List(fields.Nested(lambda: BookSchema(exclude=['author'])))
 
     @validates('birth_date')
     def validate_birth_date(self, value):
         if value > datetime.now().date():
             raise ValidationError('Birth date mus be lower than {}.'.format(datetime.now().date()))
+
+
+class BookSchema(Schema):
+    id = fields.Integer(dump_only=True)
+    title = fields.String(required=True, validate=validate.Length(max=50))
+    isbn = fields.Integer(required=True)
+    number_of_pages = fields.Integer(required=True)
+    description = fields.String()
+    author_id = fields.Integer(load_only=True)
+    author = fields.Nested(lambda: AuthorSchema(only=['id', 'first_name', 'last_name']))
+
+    @validates('isbn')
+    def validate_isbn(self, value):
+        if len(str(value)) != 13:
+            ValidationError('ISBN must contain 13 digits.')
 
 
 author_schema = AuthorSchema()
