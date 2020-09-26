947cd87b0c19c7fd00fb1a74816876b1059243de
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-09-26 11:16:49 +0200

    Add get all authors filtering

--- book_library_app/models.py
+++ book_library_app/models.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import BaseQuery
 from book_library_app import db
 from datetime import datetime
 from marshmallow import Schema, fields, validate, validates, ValidationError
+from werkzeug.datastructures import ImmutableDict
 
 
 class Author(db.Model):
@@ -32,6 +33,21 @@ class Author(db.Model):
                 column_attr = getattr(Author, key, None)
                 if column_attr is not None:
                     query = query.order_by(column_attr.desc()) if desc else query.order_by(column_attr)
+
+        return query
+
+    @staticmethod
+    def apply_filters(query: BaseQuery, params: ImmutableDict) -> BaseQuery:
+        for param, value in params.items():
+            if param not in {'fields', 'sort'}:
+                column_attr = getattr(Author, param, None)
+                if column_attr is not None:
+                    if param == 'birth_date':
+                        try:
+                            value = datetime.strptime(value, '%d-%m-%Y').date()
+                        except ValueError:
+                            continue
+                    query = query.filter(column_attr == value)
 
         return query
 
