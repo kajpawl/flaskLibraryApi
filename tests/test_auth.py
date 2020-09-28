2e7e30b99fbbb26dbb23aef2414c6bb8cf2d3fb8
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-09-28 23:46:00 +0200

    Add app and auth controller tests

---
+++ tests/test_auth.py
@@ -0,0 +1,12 @@
+def test_registration(client):
+    response = client.post('/api/v1/auth/register',
+                           json={
+                               'username': 'test',
+                               'password': '123456',
+                               'email': 'test@test.test'
+                           })
+    response_data = response.get_json()
+    assert response.status_code == 201
+    assert response.headers['Content-Type'] == 'application/json'
+    assert response_data['success'] is True
+    assert response_data['token']
